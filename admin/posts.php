c8c170cd11e2d6cc02737ebe867c18e57529f63d
Phuc <enzcoding@gmail.com>
2018-10-31 11:20:29 +0700

    Displaying posts lsit in admin

--- admin/posts.php
+++ admin/posts.php
@@ -28,7 +28,33 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
+                        <?php
+                            $query = "SELECT * FROM posts";
+                            $select_posts_admin = mysqli_query($connection,$query);
+                            while ($row = mysqli_fetch_assoc($select_posts_admin)) {
+                                $post_id = $row['post_id'];
+                                $post_author = $row['post_author'];
+                                $post_title = $row['post_title'];
+                                $post_category = $row['post_category_id'];
+                                $post_status = $row['post_status'];
+                                $post_image = $row['post_image'];
+                                $post_tags = $row['post_tags'];
+                                $post_comment_count = $row['post_comment_count'];
+                                $post_date = $row['post_date'];
+
+                                echo "<tr>";
+                                echo "<td>{$post_id}</td>";
+                                echo "<td>{$post_author}</td>";
+                                echo "<td>{$post_title}</td>";
+                                echo "<td>{$post_category}</td>";
+                                echo "<td>{$post_status}</td>";
+                                echo "<td><img class='img-responsive' src='../images/$post_image' alt=''></td>";
+                                echo "<td>{$post_tags}</td>";
+                                echo "<td>{$post_comment_count}</td>";
+                                echo "<td>{$post_date}</td>";
+                                echo "</tr>";
+                            }
+                        ?>
                             <td>ID</td>
                             <td>Author</td>
                             <td>Title</td>
@@ -38,7 +64,7 @@
                             <td>Tags</td>
                             <td>Comment</td>
                             <td>Date</td>
-                        </tr>
+
                         </tbody>
                     </table>
                 </div>
