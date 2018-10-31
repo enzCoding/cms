896653e63688fcfe4dbef1f01f5eb2e7f2318608
Phuc <enzcoding@gmail.com>
2018-10-31 10:21:51 +0700

    Create a HTML table in admin to dislay a list of posts

--- admin/category.php
+++ admin/category.php
@@ -12,9 +12,7 @@
             <!-- Page Heading -->
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">
-                        Category
-                    </h1>
+                    <h1 class="page-header">Category</h1>
                     <div class="col-xs-6">
                         <!-- Add Category Function-->
                         <?php
---
+++ admin/posts.php
@@ -0,0 +1,53 @@
+<?php include "includes/admin_header.php"?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Posts</h1>
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Author</th>
+                            <th>Title</th>
+                            <th>Category</th>
+                            <th>Status</th>
+                            <th>Image</th>
+                            <th>Tags</th>
+                            <th>Comment</th>
+                            <th>Date</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>ID</td>
+                            <td>Author</td>
+                            <td>Title</td>
+                            <td>Category</td>
+                            <td>Status</td>
+                            <td>Image</td>
+                            <td>Tags</td>
+                            <td>Comment</td>
+                            <td>Date</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+    <?php include "includes/admin_footer.php"?>
