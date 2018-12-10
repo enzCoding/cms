dedc65f2b05f3427524001e26a40afdb4c460ee4
Phuc <enzcoding@gmail.com>
2018-12-10 09:47:35 +0700

    create the login form

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -16,7 +16,23 @@
         <!-- /.form search -->
         <!-- /.input-group -->
     </div>
-
+    <!-- Login Form -->
+    <div class="well">
+        <h4>Login</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input type="text" name="username" class="form-control" placeholder="Enter Username">
+            </div>
+            <div class="input-group">
+                <input type="password" name="password" class="form-control" placeholder="Enter Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">Login</button>
+                </span>
+            </div>
+        </form>
+        <!-- /.form search -->
+        <!-- /.input-group -->
+    </div>
     <!-- Blog Categories Well -->
     <div class="well">
         <h4>Blog Categories</h4>
